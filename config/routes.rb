9d82e6c1c19e98b5de38c30950b788e18446554e
Deyka Howe <deyka.howe@gmail.com>
2018-01-10 15:15:05 -0800

    routes defined in the app routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get '/index' => 'users#index'
-  get '/show' => 'users#show'
-  get '/new' => 'users#new'
-  get '/create' => 'users#create'
-  get '/edit' => 'users#edit'
-  get '/update' => 'users#update'
-  get '/destroy' => 'users#destroy'
+  root 'users#index'
+  resources :sessions, only: [:new, :create]
+  resources :users
+  resources :events
 end
 
 
-# HOW TO USE THIS... 
-# root 'users#index'
-# resources :users
-# resources :events
