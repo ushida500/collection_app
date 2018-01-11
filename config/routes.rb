eb38b2c4489494032898c49404ed9d2bfe845b7d
Deyka Howe <deyka.howe@gmail.com>
2018-01-10 16:55:09 -0800

    a new controller is generated for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'users#index'
   resources :sessions, only: [:new, :create]
   resources :users
