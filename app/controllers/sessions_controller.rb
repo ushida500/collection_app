58b75c1239527a29c2bc3483f7762f90d97d6a8e
Deyka Howe <deyka.howe@gmail.com>
2018-01-12 09:58:49 -0800

    a flash message created in sessions_controller.rb file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,8 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
+      # if login info is incorrect...
+      flash[:danger] = 'Email or username is incorrect.'
       redirect_to new_session_path 
     end
   end
